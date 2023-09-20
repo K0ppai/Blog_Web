2261a7ab04cbd8d6f977750699cbb08ce96d0a18
K0ppai <psthu.koppai@gmail.com>
2023-09-20 16:55:33 +0630

    Set default valur to 0 for counters

---
+++ db/migrate/20230920102308_change_counter_defaults_in_posts.rb
@@ -0,0 +1,7 @@
+class ChangeCounterDefaultsInPosts < ActiveRecord::Migration[7.0]
+  def change
+    change_column_default :posts, :comments_counter, 0
+    change_column_default :posts, :likes_counter, 0
+    change_column_default :users, :post_counter, 0
+  end
+end
