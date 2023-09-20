70c3ff290b5444e67dd872d2c6f1bffec59cb29c
K0ppai <psthu.koppai@gmail.com>
2023-09-20 23:10:29 +0630

    Refactor Post model to pass tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,11 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  after_save :update_post_comment_counter
+  validates :title, presence: true, length: { in: 1..250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_save :update_user_post_counter
 
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
