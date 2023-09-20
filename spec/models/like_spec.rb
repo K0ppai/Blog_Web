8cf1425c368f8feb0e480135336408f7b7d3c06e
K0ppai <psthu.koppai@gmail.com>
2023-09-20 23:35:46 +0630

    Fix linter error

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,18 +1,18 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   before :each do
     @like = Like.new(user_id: 3, post_id: 1)
   end
 
-  context "When creating likes" do
-    it "Id of the user should be an integer" do
-      @like.user_id = ""
+  context 'When creating likes' do
+    it 'Id of the user should be an integer' do
+      @like.user_id = ''
       expect(@like).to_not be_valid
     end
 
-    it "Id of the post should be an integer" do
-      @like.post_id = "asdf"
+    it 'Id of the post should be an integer' do
+      @like.post_id = 'asdf'
       expect(@like).to_not be_valid
     end
   end
