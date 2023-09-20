7328d3fad82d0cd320e8f29f283cef9054425ef8
K0ppai <psthu.koppai@gmail.com>
2023-09-20 16:58:08 +0630

    Add method to update user's post_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   has_many :comments
   has_many :likes
+
+  def update_user_post_counter
+    author.post_counter += 1
+    author.save
+  end
 end
