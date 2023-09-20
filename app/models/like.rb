261bc1917c73dd8814d1c56a1ccaf27e15422054
K0ppai <psthu.koppai@gmail.com>
2023-09-20 17:06:27 +0630

    Fix linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: "User", foreign_key: "user_id"
-  belongs_to :post, class_name: "Post", foreign_key: "post_id"
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   def update_post_like_counter
     post.likes_counter += 1
