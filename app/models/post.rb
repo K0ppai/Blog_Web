e63ae082fa7657ee7f3ed62d262caa80cfb8b52c
K0ppai <psthu.koppai@gmail.com>
2023-09-20 18:27:23 +0630

    Change as after save methods for counters

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,15 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def update_user_post_counter
-    author.post_counter += 1
-    author.save
-  end
+  after_save :update_post_comment_counter
 
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
+  end
+
+  private
+
+  def update_user_post_counter
+    author.increment!(:post_counter)
   end
 end
