7e69730b78dedde6e29bf7c0c9462fb538b54c44
K0ppai <psthu.koppai@gmail.com>
2023-09-25 20:13:33 +0630

    Add tests for post#index route

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
+  describe 'GET /users/:user_id/posts, ' do
+    before :each do
+      get '/users/:user_id/posts'
+    end
+
+    it 'should returns http success' do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should render the correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'the response body should include the correct placeholder test' do
+      expect(response.body).to include('Posts#index')
     end
   end
 end
