a2f1f2b3a592533d52a7b053cdc4140687f62b2c
K0ppai <psthu.koppai@gmail.com>
2023-09-25 20:04:56 +0630

    Add render template test for UserController

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,31 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
+RSpec.describe "Users", type: :request do
+  describe "GET /" do
+    before :each do
+      get "/"
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it "render the correct template" do
+      expect(response).to render_template(:index)
+    end
+  end
+  
   describe "GET /users/:id" do
+    before :each do
+      get "/users/:id"
+    end
+    
     it "returns http success" do
-      get '/users/:id'
       expect(response.status).to eq(200)
+    end
+
+    it "render the correct template" do
+      expect(response).to render_template(:show)
     end
   end
 end
