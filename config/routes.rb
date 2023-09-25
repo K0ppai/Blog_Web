58a733734dc5c8891c73051408db439987f8d6aa
K0ppai <psthu.koppai@gmail.com>
2023-09-25 17:41:59 +0630

    Create user controller for root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/users/:id", to: "users#show"
 end
