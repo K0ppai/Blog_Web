7b4ad33564fdf97853f1014338b9668ab207c0b1
K0ppai <psthu.koppai@gmail.com>
2023-09-25 18:02:10 +0630

    Fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
-  
-  get "/users/:id", to: "users#show"
+
+  get '/users/:id', to: 'users#show'
   get '/users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/:id', to: 'posts#show'
 end
