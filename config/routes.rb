3c61f826dba5547e2fcafd1e4ea558459d4aa5a5
K0ppai <psthu.koppai@gmail.com>
2023-09-27 00:48:30 +0630

    Add views for '/users/:user_id/posts' route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users
-  get '/users/:id', to: 'users#show'
-  get '/users/:user_id/posts', to: 'posts#index'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:user_id/posts', to: 'posts#index', as: 'posts_of_user'
   get '/users/:user_id/posts/:id', to: 'posts#show'
 end
