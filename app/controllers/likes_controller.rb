0926080835b0b0a1020add9fd9d3cf4407e5e0da
K0ppai <psthu.koppai@gmail.com>
2023-09-28 18:01:00 +0630

    Fix linter error

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,7 @@ class LikesController < ApplicationController
     post = Post.find(params[:post_id])
     author = User.find(params[:user_id])
     like = Like.new(author: current_user, post:)
-    
+
     redirect_to user_post_path(user_id: author, id: post) if like.save
   end
 end
