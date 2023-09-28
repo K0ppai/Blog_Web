00e14f6c8abd5f7b6fd95715d4c4ae15ca420ae1
K0ppai <psthu.koppai@gmail.com>
2023-09-28 16:13:44 +0630

    Add create method for new comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,19 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
+  end
+
+  def create
+    comment = Comment.new(params.require(:comment).permit(:text))
+    post = Post.find(params[:post_id])
+    user = User.find(params[:user_id])
+    comment.author = current_user
+    comment.post = post
+
+    if comment.save
+      redirect_to user_post_path(user_id: user, id: post)
+    else
+      render :new
+    end
   end
 end
