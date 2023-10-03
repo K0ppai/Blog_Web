f5c70b02ae85921940c50c261e53979466fcf6af
K0ppai <psthu.koppai@gmail.com>
2023-10-03 14:41:42 +0630

    Revert "Fix n+1 problems"
    This reverts commit 08eba01ae2f2d5f43d3fa88543e6ce1b6a9da617.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,9 +5,6 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
-require 'capybara/rspec'
-require 'database_cleaner'
-DatabaseCleaner.strategy = :truncation
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
