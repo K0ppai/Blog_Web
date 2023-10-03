969e8bee9144be7e71a63c419affed7e165cf899
K0ppai <psthu.koppai@gmail.com>
2023-10-03 15:19:39 +0630

    Fix linter errors

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,13 +1,13 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "users/show.html.erb", type: :feature do
+RSpec.describe 'users/show.html.erb', type: :feature do
   before :each do
-    @user = User.create(name: "Koppai", photo: "https://picsum.photos/id/23/200", bio: "Micronaut")
-    @post1 = Post.create(author: @user, title: "Post 1", text: "This is how")
+    @user = User.create(name: 'Koppai', photo: 'https://picsum.photos/id/23/200', bio: 'Micronaut')
+    @post1 = Post.create(author: @user, title: 'Post 1', text: 'This is how')
     visit user_path(@user)
-    Post.create(author: @user, title: "Post 2", text: "This is how")
+    Post.create(author: @user, title: 'Post 2', text: 'This is how')
     visit user_path(@user)
-    Post.create(author: @user, title: "Post 3", text: "This is how")
+    Post.create(author: @user, title: 'Post 3', text: 'This is how')
     visit user_path(@user)
   end
 
@@ -16,36 +16,36 @@ RSpec.describe "users/show.html.erb", type: :feature do
   end
 
   it "I can see the user's username" do
-    expect(page).to have_content("Koppai")
+    expect(page).to have_content('Koppai')
   end
 
-  it "I can see the number of posts" do
+  it 'I can see the number of posts' do
     expect(page).to have_content("Number of posts: #{@user.post_counter}")
   end
 
   it "I can see the user's bio" do
-    expect(page).to have_content("Micronaut")
+    expect(page).to have_content('Micronaut')
   end
 
   it "I can see the user's first 3 posts." do
-    expect(page).to have_selector(".post", count: 3)
-    expect(page).to have_content("Post 1")
-    expect(page).to have_content("Post 2")
-    expect(page).to have_content("Post 3")
+    expect(page).to have_selector('.post', count: 3)
+    expect(page).to have_content('Post 1')
+    expect(page).to have_content('Post 2')
+    expect(page).to have_content('Post 3')
   end
 
   it "I can see a button that lets me view all of a user's posts." do
-    expect(page).to have_selector("#all-posts-btn")
-    expect(page).to have_content("See all posts")
+    expect(page).to have_selector('#all-posts-btn')
+    expect(page).to have_content('See all posts')
   end
 
   it "When I click a user's post, it redirects me to that post's show page" do
-    click_on("Post 1")
+    click_on('Post 1')
     expect(current_path).to eq(user_post_path(user_id: @user, id: @post1))
   end
 
   it "When I click to see all posts, it redirects me to the user's post's index page" do
-    click_on(id: "all-posts-btn")
+    click_on(id: 'all-posts-btn')
     expect(current_path).to eq(user_posts_path(user_id: @user))
   end
 end
