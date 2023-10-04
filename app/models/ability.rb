bf8fa97bab8fcc8b84f2ad2c200bc8e42dc8743a
K0ppai <psthu.koppai@gmail.com>
2023-10-04 22:59:45 +0630

    Update ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, Post
     return unless user.present?
 
     can :manage, Post, author_id: user.id
     can :manage, Comment, user_id: user.id
-    return unless user.role == 'admin'
+    return unless user.admin?
 
     can :manage, :all
   end
