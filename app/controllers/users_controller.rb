866bcf6bf90a01e931c53a1d1674088267286999
K0ppai <psthu.koppai@gmail.com>
2023-10-04 21:27:20 +0630

    Add sign out link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  
   def index
     @users = User.all
   end
@@ -7,5 +6,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.recent_posts
+  end
+
+  def sign_out_session
+    sign_out(current_user)
+    redirect_to root_path
   end
 end
