fe629cbe10663f81853bd36c8f96166a66d2d63c
K0ppai <psthu.koppai@gmail.com>
2023-10-04 21:26:53 +0630

    Add authorization for posts and comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,14 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, Post
     return unless user.present?
-    can :read, Post,
-    return unless user.admin?
+
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, user_id: user.id
+    return unless user.role == 'admin'
+
     can :manage, :all
   end
 end
