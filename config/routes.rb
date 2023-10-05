439b0b24e9b525a8950badbdee6471f36a5e2ca9
K0ppai <psthu.koppai@gmail.com>
2023-10-05 14:08:25 +0630

    Add destroy route for comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create destroy] do
-      resources :comments, only: %i[create]
+      resources :comments, only: %i[create destroy]
       resources :likes, only: %i[create]
     end
   end
