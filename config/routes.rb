4cebd6b4b0bb7148b1712c4d0fe01d74d2b9b773
K0ppai <psthu.koppai@gmail.com>
2023-10-05 16:59:45 +0630

    Generate CommentsController for api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :posts, only: %i[index]
+      resources :users do
+        resources :posts, only: %i[index show]
+      end
     end
   end
 
