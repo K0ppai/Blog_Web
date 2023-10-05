89e9a3ac293a6a4187649a6cb1543b1ace4ce2bc
K0ppai <psthu.koppai@gmail.com>
2023-10-05 17:54:21 +0630

    Add route for comments api

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users do
-        resources :posts, only: %i[index show]
+        resources :posts, only: %i[index] do
+          resources :comments, only: %i[index create]
+        end
       end
     end
   end
