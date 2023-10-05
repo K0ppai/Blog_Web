c619c49a5d6b16adb35c6c0c7afd4bcce22adab7
K0ppai <psthu.koppai@gmail.com>
2023-10-05 15:32:25 +0630

    Add read authorization for all

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,7 @@ class Ability
   def initialize(user)
     return unless user.present?
 
+    can :read, :all
     can :manage, Post, author_id: user.id
     can :manage, Comment, user_id: user.id
     return unless user.admin?
