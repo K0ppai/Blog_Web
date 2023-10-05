ddc05a16777fb0ce07f3533cdbb356efa957a4a6
K0ppai <psthu.koppai@gmail.com>
2023-10-05 16:58:53 +0630

    Update postscontroller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::PostsController < ApplicationController
   def index
-    @posts = Post.all
-    render json: @posts
+    @user = User.find(
+      params[:user_id]
+    )
+    @posts = @user.posts
+    render json: { success: true, data: @posts }
   end
 end
